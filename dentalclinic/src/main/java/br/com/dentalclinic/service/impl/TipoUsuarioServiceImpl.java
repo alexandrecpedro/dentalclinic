02934568612c0045206641349951e9cd5fb6c317
tpvale <tpv.tiago@gmail.com>
2022-09-08 20:45:55 -0300

    fix: TipoUsuarioServiceImpl Methods

--- dentalclinic/src/main/java/br/com/dentalclinic/service/impl/TipoUsuarioServiceImpl.java
+++ dentalclinic/src/main/java/br/com/dentalclinic/service/impl/TipoUsuarioServiceImpl.java
@@ -1,43 +1,54 @@
 package br.com.dentalclinic.service.impl;
 
+import br.com.dentalclinic.dto.EnderecoDTO;
+import br.com.dentalclinic.dto.TipoUsuarioDTO;
+import br.com.dentalclinic.model.Endereco;
 import br.com.dentalclinic.model.TipoUsuario;
 import br.com.dentalclinic.repository.ITipoUsuarioRepository;
 import br.com.dentalclinic.service.IService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class TipoUsuarioServiceImpl implements IService<TipoUsuario> {
+public class TipoUsuarioServiceImpl implements IService<TipoUsuarioDTO> {
     /** Attribute **/
     @Autowired
-    private ITipoUsuarioRepository ITipoUsuarioRepository;
-
-    /** Constructor **/
-    //public TipoUsuarioServiceImpl(TipoUsuarioRepository tipoUsuarioRepository) {
-    //    this.tipoUsuarioRepository = tipoUsuarioRepository;
-    //}
+    private ITipoUsuarioRepository tipoUsuarioRepository;
 
     /** Methods **/
     @Override
-    public TipoUsuario salvar(TipoUsuario tipoUsuario) {
+    public TipoUsuarioDTO salvar(TipoUsuarioDTO tipoUsuarioDTO) {
+        TipoUsuario tipoUsuario = mapperDTOToEntity(tipoUsuarioDTO);
         if (!tipoUsuario.equals(null)) {
-            ITipoUsuarioRepository.save(tipoUsuario);
+            tipoUsuario = tipoUsuarioRepository.save(tipoUsuario);
         }
-        return tipoUsuario;
+        tipoUsuarioDTO = mapperEntityToDTO(tipoUsuario);
+        return tipoUsuarioDTO;
     }
 
-    public List<TipoUsuario> buscarTodos(){
-        return ITipoUsuarioRepository.findAll();
+    public List<TipoUsuarioDTO> buscarTodos(){
+        List<TipoUsuario> tipoUsuarioList = tipoUsuarioRepository.findAll();
+        List<TipoUsuarioDTO> tipoUsuarioDTOS = new ArrayList<>();
+
+        for (TipoUsuario tipoUsuario: tipoUsuarioList) {
+            TipoUsuarioDTO tipoUsuarioDTO = mapperEntityToDTO(tipoUsuario);
+            tipoUsuarioDTOS.add(tipoUsuarioDTO);
+        }
+        return tipoUsuarioDTOS;
     }
 
-    public Optional<TipoUsuario> buscarById(Integer id) {
-        return ITipoUsuarioRepository.findById(id);
+    public Optional<TipoUsuarioDTO> buscarById(Integer id) {
+        TipoUsuario tipoUsuario = tipoUsuarioRepository.findById(id).get();
+        TipoUsuarioDTO tipoUsuarioDTO = mapperEntityToDTO(tipoUsuario);
+        return Optional.ofNullable(tipoUsuarioDTO);
     }
 
-    public TipoUsuario buscarByNome(String nome){return ITipoUsuarioRepository.findByNome(nome);}
+    public TipoUsuario buscarByNome(String nome) {return ITipoUsuarioRepository.findByNome(nome);}
 
     public TipoUsuario atualizar(TipoUsuario tipoUsuario) {
         return ITipoUsuarioRepository.saveAndFlush(tipoUsuario);
@@ -45,5 +56,17 @@ public class TipoUsuarioServiceImpl implements IService<TipoUsuario> {
 
     public void deletar(Integer id) {
         ITipoUsuarioRepository.deleteById(id);
+    }
+
+    public TipoUsuario mapperDTOToEntity(TipoUsuarioDTO tipoUsuarioDTO) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        TipoUsuario tipoUsuario = objectMapper.convertValue(tipoUsuarioDTO, TipoUsuario.class);
+        return tipoUsuario;
+    }
+
+    public TipoUsuarioDTO mapperEntityToDTO(TipoUsuario tipoUsuario) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        TipoUsuarioDTO tipoUsuarioDTO = objectMapper.convertValue(tipoUsuario, TipoUsuarioDTO.class);
+        return tipoUsuarioDTO;
     }
 }
