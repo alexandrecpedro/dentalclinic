0a486c890801455f82c38dd475357e0d4a3f1d1f
tpvale <tpv.tiago@gmail.com>
2022-09-22 20:59:57 -0300

    feat: Teste de Integração Mock

---
+++ dentalclinic/src/main/java/br/com/dentalclinic/utils/DentalClinicUtils.java
@@ -0,0 +1,24 @@
+package br.com.dentalclinic.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class DentalClinicUtils {
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    public static String asJsonString(final Object obj) {
+            try {
+                return mapper.writeValueAsString(obj);
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        }
+
+        public static <T> T objectFromString(Class<T> aClass, String value) {
+            try {
+                return mapper.readValue(value, aClass);
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+        }
+    }
+}
